42981336ba3a6dcf1c0574c917eb45a71c1b3ddf
Thiago Assi <thiago-assi@outlook.com>
2021-06-23 10:05:59 -0300

    added characters and artifacts sets

---
+++ utils/ArtifactsSets.js
@@ -0,0 +1,45 @@
+/*
+    INFO COLLECTED FROM:
+    Genshin Impact Wiki - Fandom
+    https://genshin-impact.fandom.com/wiki/Artifacts/Sets
+
+    These are the names of all the artifacts sets in the game.
+*/
+
+const ArtifactsSets = [
+  'Adventurer',
+  'Lucky Dog',
+  'Traveling Doctor',
+  'Resolution of Sojourner',
+  'Tiny Miracle',
+  'Berserker',
+  'Instructor',
+  'The Exile',
+  'Defender\'s Will',
+  'Brave Heart',
+  'Martial Artist',
+  'Gambler',
+  'Scholar',
+  'Prayers for Illumination',
+  'Prayers for Destiny',
+  'Prayers for Wisdom',
+  'Prayers to Springtime',
+  'Gladiator\'s Finale',
+  'Wanderer\'s Troupe',
+  'Thundersoother',
+  'Thundering Fury',
+  'Maiden Beloved',
+  'Viridescent Venerer',
+  'Crimson Witch of Flames',
+  'Lavawalker',
+  'Noblesse Oblige',
+  'Bloodstained Chivalry',
+  'Archaic Petra',
+  'Retracing Bolide',
+  'Blizzard Strayer',
+  'Heart of Depth',
+  'Tenacity of the Millelith',
+  'Pale Flame',
+];
+
+module.exports = ArtifactsSets;
